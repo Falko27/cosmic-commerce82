48c615e7cab9f6df0c75fdc2c0c8400b0d836665
Falko27 <dan.faulkner8200@gmail.com>
2023-12-09 17:32:27 +0000

    fix: add accordions

--- assets/faq-accordion.js
+++ assets/faq-accordion.js
@@ -1,1 +1,41 @@
-console.log('faq-accordion')
+class FaqAccordion extends HTMLElement {
+  constructor() {
+    super();
+
+    this.initfaq()
+  }
+
+  initfaq() {
+
+    const clear = this.clearOpenAccordions
+
+    const blocks = document.querySelectorAll('.faq-container__block')
+
+    blocks.forEach((block) => {
+
+      const header = block.querySelector('.faq-container__block__header')
+      const copy = block.querySelector('.faq-container__block__copy')
+
+      header.addEventListener('click', () => {
+
+        if (copy.classList.contains('show')) {
+          copy.classList.remove('show') 
+        } else {
+          clear()
+          copy.classList.toggle('show')
+        }
+    
+      })
+
+    })
+  }
+
+  clearOpenAccordions() {
+    const accordions = document.querySelectorAll('.faq-container__block__copy')
+    accordions.forEach((accordion) => {
+      accordion.classList.remove('show')
+    })
+  }
+}
+
+customElements.define('faq-accordion', FaqAccordion);
